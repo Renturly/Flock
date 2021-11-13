6b3641cac009c431bf3cd05e09fa093daec00ec0
Chief Bird Dog <72176320+Renturly@users.noreply.github.com>
2021-11-13 11:29:08 -0700

    JSObject
    Cash Buyer is a Superhero

---
+++ 3101.php
@@ -0,0 +1,29 @@
+<script src="bower_components/mixpanel/mixpanel-jslib-snippet.min.js"></script>
+
+<script type="text/javascript">
+mixpanel.track("$7,418",{
+			referrer: document.referrer,
+		});
+</script>
+		
+<script type="text/javascript">
+	const UrlyBird = {
+  	email: "Reidsgroupllc@gmail.com",
+  	username: "Reids Group",
+  	urlybird: "https://www.accesstheflock.io/the-urly-bird/2646",
+  	flock: "Wholesalers can make $7,418",
+  	content: " This is my Bird Dog Fee Agreement. I know most Bird Dogs don't want to pay a fee to close a deal. I have ML for Bird Dog Jobs that says that you can get a 27% discount off the Minimum Purchase Price.",
+  	Pull: "accesstheflock.com",
+  	Pay: function() {
+    	return this.username + " " + 			this.flock;
+  	}
+	};
+
+/* JSObjects are Flock Gold Chain */
+document.write(UrlyBird.Pay());
+document.write(UrlyBird.content);
+
+mixpanel.track("Wholesalers can make $7,418",{
+			referrer: document.referrer,
+		});
+</script>
