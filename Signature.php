920a6042b4211fd9a543e4814af2d59b721fe50c
Chief Bird Dog <72176320+Renturly@users.noreply.github.com>
2021-11-24 12:20:40 -0700

    Birds of a Feather
    Take a Picture - it’s Instagram

---
+++ Signature.php
@@ -0,0 +1,19 @@
+
+
+<style> 
+input {
+font-family: 'Comforter Brush', cursive;
+}
+</style>
+
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&display=swap" rel="stylesheet">
+
+Messenger <input type="text" id="messenger">
+Name <input type="text" id="name">
+Signature <input type="text" id="signature">
+
+<br>
+
+<p>* Take a picture of your Signature and paste it onto <a href="http://renturly.com">Renturly</a></p>
